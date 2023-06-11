b4e9e720925c38f596d38df20c1bd322fc5f979e
Jacopo <jacopo.ceccuti@gmail.com>
2023-06-11 18:37:19 +0200

    Added time and date class and some minor changes

--- Test/DoublePump&Temperature.py
+++ Test/DoublePump&Temperature.py
@@ -3,7 +3,7 @@ from time import sleep
 import Temp as tp
 import utime
 
-print("\n Mixer and Thermometer test 1.2.1")
+print("\n Mixer and Thermometer test 1.2.3")
 
 step1 = PWM(Pin(12))
 step2 = PWM(Pin(27))
@@ -12,13 +12,36 @@ direc1 = Pin(33, Pin.OUT)
 direc2 = Pin(15, Pin.OUT)
 
 direc1.value(1)
-direc2.value(1)
+direc2.value(0)
 
-step1.freq(5000)
-step2.freq(7000)
+step1.freq(4000)
+step2.freq(12000)
 
 step1.duty(1023)
 step2.duty(1023)
+
+
+# while True:
+#     #direc1.value(0)
+#     direc2.value(0)
+#     direc1.value(0)
+
+#     #step1.duty(1023)
+#     step2.duty(1023)
+#     step1.duty(1023)
+    
+#     sleep(5)
+    
+#     direc2.value(1)
+#     direc1.value(1)
+
+#     sleep(5)
+
+#     step2.duty(0)
+#     step1.duty(0)
+
+#     sleep(2)
+
 
 
 print("\nThe thermometer alive!")
---
+++ Test/Interrupt.py
@@ -0,0 +1,9 @@
+# PinInterrupt = Pin(00, Pin.IN)    # TODO: Def. pin 
+
+# Interrupt = False
+
+# def button_pushed():
+#     global Interrupt
+#     Interrupt = True
+
+# PinInterrupt.irq(trigger = Pin.IRQ_FALLING | Pin.IRQ_RISING, handler = button_pushed)
